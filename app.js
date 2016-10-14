30e9eb286a1e6b9d838ed271e96a69278e7214c7
lunzhang <lun.zhang1993@gmail.com>
2016-10-14 16:39:29 -0400

    init commit for ng2

--- app.js
+++ app.js
@@ -11,19 +11,16 @@ var passport = require('passport');
 var routesApi = require('./api/routes/index');
 var app = express();
 
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(express.static(path.join(__dirname, 'ng')));
-app.use(express.static(path.join(__dirname, 'ng2')));
-
 app.use('/api', routesApi);
 app.use('/', routes);
+app.use('/',express.static(path.join(__dirname, 'ng')));
+app.use('/ng2',express.static(path.join(__dirname, 'ng2')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- gulpfile.js
+++ gulpfile.js
@@ -11,62 +11,75 @@ var livereload = require('gulp-livereload');
 var uglify = require('gulp-uglify');
 
 var paths = {
-    sass: ['./ng/**/*.scss'],
-    controllers: ['./ng/**/*.js']
+  sass: ['./ng/**/*.scss','./ng2/**/*.scss'],
+  controllers: ['./ng/**/*.js']
 };
 
 gulp.task('controllers', function () {
-    return gulp.src(['ng/controllers/*.js', 'ng/auth/**/*.js'])
-    .pipe(concat('controllers.min.js'))
-    .pipe(uglify())
-    .on('error', gutil.log)
-    .pipe(gulp.dest('ng'));
+  return gulp.src(['ng/controllers/*.js', 'ng/auth/**/*.js'])
+  .pipe(concat('controllers.min.js'))
+  .pipe(uglify())
+  .on('error', gutil.log)
+  .pipe(gulp.dest('ng'));
 });
 
 gulp.task('sass', function (done) {
-    gulp.src('./ng/css/*.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./ng/css/'))
-    .pipe(minifyCss({
-        keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./ng/css/'))
-    .on('end', done);
+  gulp.src('./ng/css/*.scss')
+  .pipe(sass())
+  .on('error', sass.logError)
+  .pipe(gulp.dest('./ng/css/'))
+  .pipe(minifyCss({
+    keepSpecialComments: 0
+  }))
+  .pipe(rename({ extname: '.min.css' }))
+  .pipe(gulp.dest('./ng/css/'))
+  .on('end', done);
+});
+
+gulp.task('sass2',function(done){
+  gulp.src('./ng2/css/*.scss')
+  .pipe(sass())
+  .on('error', sass.logError)
+  .pipe(gulp.dest('./ng2/css/'))
+  .pipe(minifyCss({
+    keepSpecialComments: 0
+  }))
+  .pipe(rename({ extname: '.min.css' }))
+  .pipe(gulp.dest('./ng2/css/'))
+  .on('end', done);
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.sass, ['sass']);
-    gulp.watch(paths.controllers, ['controllers']);
+  gulp.watch(paths.sass, ['sass','sass2']);
+  gulp.watch(paths.controllers, ['controllers']);
 });
 
 gulp.task('default', ['watch'], function () {
-    nodemon({
-        // the script to run the app
-        script: './bin/www.js',
-        ext: 'js'
-    })
-    .on('restart', function () {
-    });
+  nodemon({
+    // the script to run the app
+    script: './bin/www.js',
+    ext: 'js'
+  })
+  .on('restart', function () {
+  });
 });
 
 gulp.task('install', ['git-check'], function () {
-    return bower.commands.install()
-    .on('log', function (data) {
-        gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+  return bower.commands.install()
+  .on('log', function (data) {
+    gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+  });
 });
 
 gulp.task('git-check', function (done) {
-    if (!sh.which('git')) {
-        console.log(
-            '  ' + gutil.colors.red('Git is not installed.'),
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
       '\n  Git, the version control system, is required to download Ionic.',
       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-        );
-        process.exit(1);
-    }
-    done();
+    );
+    process.exit(1);
+  }
+  done();
 });
